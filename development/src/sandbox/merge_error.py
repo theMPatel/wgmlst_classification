4e41e3b0210cbf66559717a9457af342bb32b56b
Milan Patel <mpatel5@cdc.gov>
2018-01-05 15:37:39 -0500

    Decided on merge sequence

--- development/src/sandbox/merge_error.py
+++ development/src/sandbox/merge_error.py
@@ -1,13 +1,21 @@
+#!/usr/bin/env python
+
 import os
 import sys
 from itertools import combinations
 
 
-test_list = [ 1, 2 ,3 ,4 ,5 ]
+test_list = [ 1, 2, 3 ]
 
 
-for i in range(len(test_list), -1, -1):
+# Prefer the early merges, if they are within acceptable ranges
+for i in range(len(test_list), 0, -1):
 
     for combo in combinations(test_list, i):
 
         print(combo)
+
+        # if err( combo ) < max_acceptable_err:
+        #   MERGE( combo )
+        # else:
+        #   continue
