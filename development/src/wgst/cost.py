9c8cd2d2fe0a5c0d71ba6da409e157ab77719b70
Milan Patel <mpatel5@cdc.gov>
2018-01-09 14:52:29 -0500

    Added cost functions file

--- development/src/wgst/cost.py
+++ development/src/wgst/cost.py
@@ -1,8 +1,16 @@
+##############################################################################
+# Date: 1/9/2018
+# Author: Milan Patel
+# Cost functions
+##############################################################################
 
+from itertools import combinations
 
+__all__ = ['mean_error', 'mean_inter_error', 'ABSOLUTE_MARGIN_ERROR', 'SQUARED_ERROR']
+
+# This is for Listeria, you will need to change this at runtime
 ABSOLUTE_MARGIN_ERROR = 0.2860411899
 SQUARED_ERROR = -1
-
 
 def mean_error(unnamed, key_list, d_matrix, thresholds, level, 
     biased=True, squared=False):
@@ -30,3 +38,48 @@ def mean_error(unnamed, key_list, d_matrix, thresholds, level,
     else:
         return 0.
 
+def mean_inter_error(unnamed, key_list, d_matrix, thresholds, level,
+    biased=True, squared=False):
+
+    
+    results = []
+
+    for entry, other in combinations(key_list, 2):
+
+        err = thresholds[level-1]-d_matrix[entry,other]
+
+        if err >= 0.:
+
+            if biased:
+                continue
+            else:
+                results.append(0.)
+
+        elif squared:
+            results.append(err**2)
+
+        else:
+            results.append(err)
+
+    for key in key_list:
+
+        err = thresholds[level-1]-d_matrix[unnamed,key]
+
+        if err >= 0.:
+
+            if biased:
+                continue
+            else:
+                results.append(0.)
+
+        elif squared:
+            results.append(err**2)
+
+        else:
+            results.append(err)
+
+    if len(results):
+        return abs(sum(results))/float(len(results))
+
+    else:
+        return 0.
